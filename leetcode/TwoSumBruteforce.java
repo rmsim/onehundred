d91e22530e7d62fd18404fc202da84e8c87fbcda
Rui Min Sim <rmsim@dyn-160-39-133-110.dyn.columbia.edu>
2017-09-26 14:07:37 -0400

    Renamed TwoSum to TwoSumBruteforce

---
+++ leetcode/TwoSumBruteforce.java
@@ -0,0 +1,22 @@
+/*
+ * Brute Force Method
+ * Time complexity: O(n^2)
+ * Because for each element, we try to find its complement by looping
+ * the rest of the array in O(n) time. Inner loop = n, outerloop = n.
+ * 
+ * Space complexity: O(1)
+ */
+class TwoSumBruteforce {
+
+  public int[] twoSum(int[] nums, int target) {
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i+1; j < nums.length; j++) {
+        if (nums[j] == target - nums[i]) {
+          return new int[] {i, j};
+        }
+      }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+}
